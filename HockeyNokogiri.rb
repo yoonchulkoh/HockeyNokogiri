5f85537d25cfe0dcb5f89607ee582541d751a04a
yoonchul.koh <yoonchul.koh@gmail.com>
2015-01-08 00:03:00 +0900

    ベタ書きからクラスに書き換え

--- HockeyNokogiri.rb
+++ HockeyNokogiri.rb
@@ -3,132 +3,161 @@ require 'nokogiri'
 require 'mechanize'
 require 'csv'
 
-$config = YAML.load_file('config.yml')
+class HockeyNokogiri
 
-if $config['app_id'].nil? || $config['app_id'].empty?
-  puts "app_id is not found in config file."
-  exit
-end
-if $config['version'].nil? || $config['version'].empty?
-  puts "version is not found in config file."
-  exit
-end
-if $config['email'].nil? || $config['email'].empty?
-  puts "email is not found in config file."
-  exit
-end
-if $config['password'].nil? || $config['password'].empty?
-  puts "password is not found in config file."
-  exit
-end
+  CONFIG_FILE_NAME = 'config.yml'
+  DEBUG = false
 
-# スクレイピング先のURL
-url = "https://rink.hockeyapp.net/manage/apps/#{$config['app_id']}/app_versions/#{$config['version']}/crash_reasons?per_page=50&order=desc&sort_by=count&type=symbols"
+  @config
+  @agent
 
-charset = nil
-html = open(url) do |f|
-  charset = f.charset
-  f.read
-end
-
-# htmlをパース(解析)してオブジェクトを生成
-doc = Nokogiri::HTML.parse(html, nil, charset)
-
-
-agent = Mechanize.new
-
-puts '### ログイン開始'
-
-agent.get(url) do |page| 
-  # ログインする
-  login_result = page.form_with(id: 'sign_in') do |login|
-    login['user[email]'] = $config['email']
-    login['user[password]'] = $config['password']
-  end.submit
-end
-
-puts '### ログイン完了'
-
-puts '### 詳細ページURL取得開始'
-
-# 詳細ページURLを全て取得する
-$rel_list = []
-$description_list = []
-
-def get_rel_list(agent, url, page)
-  agent.get(url + "&page=#{page}") do |page|
-    html = Nokogiri::HTML(page.body)
-    return false if html.css('table.crash_reasons tr') == nil
-    html.css('table.crash_reasons tr').each do |tr|
-      if tr['rel'] == nil
-        next
-      else
-        rel = tr['rel']
-        $rel_list << rel
-      end
-      $description_list << tr.css('td.description').text.strip.gsub(/\n/, "") unless tr.css('td.description') == nil
-    end
+  def execute
+    init
+    doc = get_document
+    do_login
+    rel_list, description_list = get_detail_page_urls
+    crash_list = get_crash_list(rel_list, description_list)
+    export_csv(crash_list)
   end
-  return true
-end
+  
+  def init
+    load_config
+    if error = check_config
+      puts error
+      return
+    end
+    @agent = Mechanize.new
+  end
 
-# ページング判定は面倒なのでとりあえずMAX10までで
-1.upto(10) do |i|
-  res = get_rel_list(agent, url, i)
-  next unless res
-end
+  def load_config
+    @config = YAML.load_file(CONFIG_FILE_NAME)
+  end
 
-puts "### 詳細ページURL取得完了 #{$rel_list.count}件"
+  def check_config
+    ['app_id', 'version', 'email', 'password'].each do |key|
+      if @config[key].nil? || @config[key].empty?
+        return "#{key} is not found in config file."
+      end
+    end
+    return nil
+  end
 
+  def get_document
+    charset = nil
+    html = open(get_url) do |f|
+      charset = f.charset
+      f.read
+    end
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+  end
 
-puts '### クラッシュ一覧取得開始'
+  def get_url
+    "https://rink.hockeyapp.net/manage/apps/#{@config['app_id']}/app_versions/#{@config['version']}/crash_reasons?per_page=50&order=desc&sort_by=count&type=symbols"
+  end
 
-# 詳細ページからテーブル内容を取得する
-$crash_list = []
+  def do_login
+    puts '### ログイン開始'
+    @agent.get(get_url) do |page| 
+      login_result = page.form_with(id: 'sign_in') do |login|
+        login['user[email]'] = @config['email']
+        login['user[password]'] = @config['password']
+      end.submit
+    end
+    puts '### ログイン完了'
+  end
 
-def get_crash_list(agent, url, page, reason)
-  agent.get(url + "&page=#{page}") do |page|
-    html = Nokogiri::HTML(page.body)
-    return false if html.css('table.crashes tr') == nil
-    html.css('table.crashes tr.crash').each do |tr|
-      tds = []
-      tr.css('td').each do |td|
-        unless td.text.strip.empty?
-          tds << td.text.strip.gsub(/\n/, "")
+  def get_detail_page_urls
+    puts '### 詳細ページURL取得開始'
+    rel_list = []
+    description_list = []
+
+    # ページング判定は面倒なのでとりあえずMAX10までで
+    1.upto(10) do |i|
+      res = get_detail_page_urls_per_page(@agent, get_url, i)
+      break if DEBUG
+      next unless res
+      rel_list.concat(res[0])
+      description_list.concat(res[1])
+    end
+    puts "### 詳細ページURL取得完了 #{rel_list.count}件"
+    return rel_list, description_list
+  end
+
+  def get_detail_page_urls_per_page(agent, url, page_num)
+    rel_list = []
+    description_list = []
+    agent.get(url + "&page=#{page_num}") do |page|
+      html = Nokogiri::HTML(page.body)
+      return false if html.css('table.crash_reasons tr') == nil
+      html.css('table.crash_reasons tr').each_with_index do |tr, i|
+        if tr['rel'] == nil
+          next
         else
-          span = td.css('span')
-          if span.nil? || span.empty?
-            tds << ""
+          rel = tr['rel']
+          rel_list << rel
+        end
+        description_list << tr.css('td.description').text.strip.gsub(/\n/, "") unless tr.css('td.description') == nil
+        break if DEBUG
+      end
+    end
+    return rel_list, description_list
+  end
+
+  def get_crash_list(rel_list, description_list)
+    crash_list = []
+    rel_list.each_with_index do |url, i|
+      puts "#{i+1}件/#{rel_list.count}件"
+      reason = description_list[i]
+      1.upto(10) do |i|
+        res = get_crash_list_per_page(@agent, "https://rink.hockeyapp.net#{url}?order=desc&per_page=50&sort_by=user&type=crashes", i, reason)
+        break if DEBUG
+        next unless res
+        crash_list.concat(res)
+      end
+    end
+    crash_list
+  end
+
+  def get_crash_list_per_page(agent, url, page, reason)
+    crash_list = []
+    agent.get(url + "&page=#{page}") do |page|
+      html = Nokogiri::HTML(page.body)
+      return false if html.css('table.crashes tr') == nil
+      html.css('table.crashes tr.crash').each do |tr|
+        tds = []
+        tr.css('td').each do |td|
+          unless td.text.strip.empty?
+            tds << td.text.strip.gsub(/\n/, "")
           else
-            tds << span.attr('data-value')
+            span = td.css('span')
+            if span.nil? || span.empty?
+              tds << ""
+            else
+              tds << span.attr('data-value')
+            end
           end
         end
+        tds << reason
+        crash_list << tds unless tds.nil?
+        break if DEBUG
       end
-      tds << reason
-      $crash_list << tds unless tds.nil?
     end
+    return crash_list
   end
-  return true
-end
 
-$rel_list.each_with_index do |url, i|
-  puts "#{i+1}件/#{$rel_list.count}件"
-  reason = $description_list[i]
-  1.upto(10) do |i|
-    res = get_crash_list(agent, "https://rink.hockeyapp.net#{url}?order=desc&per_page=50&sort_by=user&type=crashes", i, reason)
-    next unless res
-  end
-end
-
-puts '### クラッシュ一覧取得終了'
-
-puts '### CSV出力開始'
-
-CSV.open("crash_report_#{DateTime.now.strftime("%Y%m%d%H%M%S")}.csv", "wb") do |csv|
-  csv << ['Device','OS','Jailbroken Device','Description Attached','User','Contact','Date','-','Description']
-    $crash_list.each do |crash|
+  def export_csv(crash_list)
+    puts '### CSV出力開始'
+    CSV.open("crash_report_#{DateTime.now.strftime("%Y%m%d%H%M%S")}.csv", "wb") do |csv|
+      csv << ['Device','OS','Jailbroken Device','Description Attached','User','Contact','Date','-','Description']
+      crash_list.each do |crash|
         csv << crash
+      end
     end
+    puts '### CSV出力完了'
+  end
+
 end
 
-puts '### CSV出力完了'
+
+hockeyNokogiri = HockeyNokogiri.new
+hockeyNokogiri.execute
